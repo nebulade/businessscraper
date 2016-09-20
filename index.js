83b748a9f6d714013ec2763d5ca73b4b133ffa73
Johannes Zellner <johannes@nebulon.de>
2016-09-20 12:56:40 +0200

    Fix responsive check

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var Crawler = require('simplecrawler'),
+    request = require('request'),
     cheerio = require('cheerio');
 
 const BASE_PAGE = '';
@@ -43,13 +44,13 @@ crawler.on('fetchcomplete', function (queueItem, responseBuffer) {
         // ignore if we can't get the name
         if (!name) return;
 
-        console.error('');
-        console.error('Name:', name);
-        console.error('Street:', street);
-        console.error('Location: %s - %s', postalCode, locality);
-        console.error('Phone:', phone);
-        console.error('Website:', website);
-        console.error('');
+        // console.error('');
+        // console.error('Name:', name);
+        // console.error('Street:', street);
+        // console.error('Location: %s - %s', postalCode, locality);
+        // console.error('Phone:', phone);
+        // console.error('Website:', website);
+        // console.error('');
 
         var output = {
             name: name,
@@ -63,25 +64,19 @@ crawler.on('fetchcomplete', function (queueItem, responseBuffer) {
 
         if (website) {
             var responsive = false;
-            var responsiveCrawler = new Crawler(website);
-            responsiveCrawler.maxDepth = 1;
-
             ++responsiveCrawlerCount;
 
-            responsiveCrawler.on('fetchcomplete', function (queueItem, responseBuffer) {
-                var $ = cheerio.load(responseBuffer.toString('utf8'));
-                responsive = !!$('meta[name="viewport"]').contents().length;
-            });
-
-            responsiveCrawler.on('complete', function () {
-                output.responsive = responsive;
-                results.push(output);
+            request({ uri: website }, function (error, response, body) {
                 --responsiveCrawlerCount;
+                var $ = cheerio.load(body);
+
+                responsive = !!$('meta[name="viewport"]').length;
+                output.responsive = responsive;
+
+                results.push(output);
 
                 end();
             });
-
-            responsiveCrawler.start();
         } else {
             results.push(output);
         }
@@ -89,7 +84,7 @@ crawler.on('fetchcomplete', function (queueItem, responseBuffer) {
 });
 
 function end() {
-    if (mainDone && responsiveCrawlerCount > 0) return;
+    if (!mainDone || responsiveCrawlerCount > 0) return;
     console.log(JSON.stringify({ results: results }, null, 4));
 }
 
